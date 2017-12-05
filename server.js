f0ce14fc0d504a175a2f5870a7fb90be150e9697
Lisa Jetton <ldjetton21@gmail.com>
2017-12-05 14:30:33 -0600

    updates to structre and db structure

---
+++ server.js
@@ -0,0 +1,7 @@
+//This if the MongoDB URI String (for use in the Production Env, as ref/use as the Env Var) ==> heroku_dr61zkxg:m0lf5nkldjfi0cjmhunrth12c8@ds127389.mlab.com:27389/heroku_dr61zkxg
+
+//REVIEW WHAT THIS MEANS: Then, just pass the MONGODB_URI variable to mongoose.connect. If you define MONGODB_URI on heroku, your production app will automatically use the remote database.
+
+
+//gs-c-promo-body gel-1/2@xs gel-1/1@m gs-u-mt@m
+//gs-c-promo-image gs-u-display-none gs-u-display-inline-block@xs gel-1/2@xs gel-1/1@m
