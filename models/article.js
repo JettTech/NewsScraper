a50af710852df928017481ee9ee87efc3d478aa2
Lisa Jetton <ldjetton21@gmail.com>
2017-12-07 15:54:36 -0600

    restructure + handlebars

---
+++ models/article.js
@@ -0,0 +1,45 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new UserSchema object..
+// .., this is similar to a Sequelize model ~= to SQL Table
+var ArticleSchema = new UserSchema ({
+		title: {
+			type: String,
+			require: true
+		},
+
+		link: {
+			type: String,
+			required: true
+		},
+
+		img: {
+			type: String,
+			required: true
+		},
+
+		descriptionShort: {
+			type: String,
+			required: true
+		},
+
+		descriptionFull: String,
+
+		note: {
+			type: Schema.Types.ObjectId,
+			ref: "Note"
+		},
+		  // `Note` is an Mongoose object (model), and therefore stores its own Note id
+  			// The ref property links the ObjectId to the Note model
+  				// This allows us to populate the Article with an associated Note Model
+
+		date: {
+			type: date,
+			required: false
+		}
+});
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
+
--- server.js
+++ server.js
@@ -1,7 +1,65 @@
-//This if the MongoDB URI String (for use in the Production Env, as ref/use as the Env Var) ==> heroku_dr61zkxg:m0lf5nkldjfi0cjmhunrth12c8@ds127389.mlab.com:27389/heroku_dr61zkxg
+//This if the MongoDB URI String (for use in the Production Env, as ref,
+   //use as the Env Var) ==> heroku_dr61zkxg:m0lf5nkldjfi0cjmhunrth12c8@ds127389.mlab.com:27389/heroku_dr61zkxg
 
-//REVIEW WHAT THIS MEANS: Then, just pass the MONGODB_URI variable to mongoose.connect. If you define MONGODB_URI on heroku, your production app will automatically use the remote database.
+//REVIEW: Then, just pass the MONGODB_URI variable to mongoose.connect. If you define MONGODB_URI on heroku,
+// your production app will automatically use the remote database.
 
 
-//gs-c-promo-body gel-1/2@xs gel-1/1@m gs-u-mt@m
-//gs-c-promo-image gs-u-display-none gs-u-display-inline-block@xs gel-1/2@xs gel-1/1@m
+
+//DEPENDENCIES:
+//===============================================
+var express = require("express");
+var app = express();
+
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var request = require("request"); //>>REVIEW THE PURPOSE OF THIS dependency AGAIN...
+
+var logger = require("morgan");
+var cheerio = require("cheerio"); //Scraping-Tool Dependency
+var expressHandlebars = require("express-handlebars");
+
+>>require("./models")(app); //LOCAL FILE
+
+//var path = require("path"); >> DO WE NEED THIS DEPENDENCY/ PACKAGE?
+
+
+//Server SET-UP (PORT, route middleware, and (general) dependency configuration.);
+//================================================================================
+var PORT = process.env.PORT | 4500;
+
+
+app.use(logger("")) // Use morgan logger for logging requests
+app.use(bodyParser.urlencoded({extended: false})); // code determins how to handle form
+ //submissions, false denies extended objects (objects whose keys have values that are 
+  // addt'l ojbs..) to display, will return them undefined.
+app.use(express.static("public")); 
+  //serves the "PUBLIC" folder as the static folder/directory for entire app.
+
+
+//Est MongoDB Set-up, and ConnecT to Mongo Database (Routing)
+//=================================================
+mongoose.Promise = Promise;
+// mongoose.connect("mongodb://localhost/{NAME OF DB GOES HERE}", { // Connect to the Mongo DB
+// 	useMongoClient: true //What is the difference between using this and the following code example???
+// });
+
+
+//Local Dependencies/Imports:
+//=================================================
+
+//Routes:
+//=================================================
+
+// A GET route for scraping the echojs website
+
+var scrape
+
+
+
+
+// Start the server
+//=================================================
+var PORT = app.listen(PORT, function() {
+	console.log("We're listening on PORT: " + PORT);
+});
